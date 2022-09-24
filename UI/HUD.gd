b745cd604e1a65175ff3019029556639c2346195
lvanjelg <lukevanjelg@gmail.com>
2022-09-24 19:54:31 -0400

    Project2

---
+++ UI/HUD.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var lives_pos = Vector2.ZERO
+var lives_index = 30
+onready var Indicator = load("res://UI/Indicator.tscn")
+
+func _ready():
+	lives_pos.x = 20
+	lives_pos.y = Global.Sc.y - 20
+	update_score()
+	update_time()
+	update_lives()
+
+func update_score():
+	$Score.text = "Score: " + str(Global.score)
+	
+func update_time():
+	$Time.text = "Time Remaining: " + str(Global.time)
+	
+func update_lives():
+	for child in $Indicator_Container.get_children():
+		child.queue_free()
+	for i in range(Global.lives):
+		var indicator = Indicator.instance()
+		indicator.position = Vector2(lives_pos.x+i*lives_index,lives_pos.y)
+		$Indicator_Container.add_child(indicator)
+
+
+func _on_Timer_timeout():
+	if Global.time < 0:
+		var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	Global.time -= 1 
+	update_time()
+	
